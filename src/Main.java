8946f6542832eb319f117b4c4dae12dd4506b29a
Lukasz Adamczyk <lu.adamczyk97@gmail.com>
2022-06-28 13:04:55 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        boolean k = TeenNumberChecker.hasTeen(19, 20, 40);
-        System.out.println(k);
-        boolean f = TeenNumberChecker.isTeen(11);
-        System.out.println(f);
+        // check if there is a teen number
+        boolean isTeenNumberHere = TeenNumberChecker.hasTeen(19, 20, 40);
+        System.out.println(isTeenNumberHere);
+        // check if number is greater then 13 and smaller then 19 inclusively
+        boolean isThisNumberATeenNumber = TeenNumberChecker.isTeen(11);
+        System.out.println(isThisNumberATeenNumber);
     }
 }
